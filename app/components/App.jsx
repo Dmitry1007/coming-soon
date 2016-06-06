eae4c8994c11db182a3378b8f0735dba18659d6a
Dmitry Sky <dmitry.sky@icloud.com>
2016-06-06 10:55:29 -0600

    email component working like buttah

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,24 +4,44 @@ import Email from './Email.jsx'
 
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'info@yourdomain.com'
-        }
-      ]
-    };
-  }
+  //   this.state = {
+  //     notes: [
+  //       {
+  //         id: uuid.v4(),
+  //         task: 'info@yourdomain.com'
+  //       }
+  //     ]
+  //   };
+  // }
 
   render() {
-    const notes = this.state.notes;
+    // const notes = this.state.notes;
     return (
       <div>
-        <Email />
+        <header>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-6">
+                <div className="logo wow bounceInLeft" data-wow-delay="0.4s">
+                  <a className="site-logo" href="index.html">Ino Soon</a>
+                </div>
+              </div>
+                <div className="col-sm-6">
+                  <ul className="contact-list wow bounceInRight" data-wow-delay="0.4s">
+                    <li><i className="fa fa-phone"></i>
+                      1 -234 -456 -7890
+                    </li>
+                    <li>
+                      <Email />
+                    </li>
+                  </ul>
+                </div>
+            </div>
+          </div>
+        </header>
       </div>
     );
   }
--- app/components/Email.jsx
+++ app/components/Email.jsx
@@ -32,26 +32,10 @@ export default class Email extends React.Component {
 
   renderEmail = () => {
     return (
-      <header>
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-6">
-              <div className="logo wow bounceInLeft" data-wow-delay="0.4s">
-                <a className="site-logo" href="index.html">Ino Soon</a>
-              </div>
-            </div>
-            <div className="col-sm-6">
-                <ul className="contact-list wow bounceInRight" data-wow-delay="0.4s">
-                  <li><i className="fa fa-phone"></i> 1 -234 -456 -7890</li>
-                  <li>
-                    <i className="fa fa-envelope"></i>
-                      <span onClick={this.edit} className="task">{this.state.address}</span>
-                    </li>
-                </ul>
-            </div>
-          </div>
-        </div>
-      </header>
+      <div onClick={this.edit}>
+        <i className="fa fa-envelope"></i>
+        {this.state.address}
+      </div>
     );
   };
 
